885d4ac86b16d205cafb983ddb5bf9791e55700b
Matthew D Wilson <mattrick2214@gmail.com>
2025-03-21 14:46:35 -0500

    parseFloat

--- typescript_project/dist/index.js
+++ typescript_project/dist/index.js
@@ -1,8 +1,17 @@
-let studnet = "John";
+// Type annotations aren't necessary when Typescript can infer
+let student = "John";
 let grade1 = 8;
 let grade2 = 9;
+// When it can't, it is recommended to explicitly define the type
 let isApproved;
+// Type annotation for function arguments
 function average(num1, num2) {
     return (num1 + num2) / 2;
 }
-console.log(average(grade1, grade2));
+let grades = ["8", "7", "9", "11", "6"];
+function finalGrade(grades) {
+    let sum = 0;
+    grades.forEach(grade => sum += parseFloat(grade));
+    return sum / grades.length;
+}
+console.log(finalGrade(grades));
